596271ab1e563cd1d8d8e7b476d90275f93db0e0
Alex Wilson <fonzmp@gmail.com>
2018-06-01 19:50:06 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("Tasklister", "Self Help", 1500, "03/05/2018", "09/05/2018"),
+("Tasklister", "Self Help", 1500, "03/05/2018", "09/05/2018"),
+("Tasklister", "Self Help", 1500, "03/05/2018", "09/05/2018"),
+("Tasklister", "Self Help", 1500, "03/05/2018", "09/05/2018"),
+("Tasklister", "Self Help", 1500, "03/05/2018", "09/05/2018"),
+("Tasklister", "Self Help", 1500, "03/05/2018", "09/05/2018"),
+("Tasklister", "Self Help", 1500, "03/05/2018", "09/05/2018"),
+("Tasklister", "Self Help", 1500, "03/05/2018", "09/05/2018"),
+("Tasklister", "Self Help", 1500, "03/05/2018", "09/05/2018"),
+("Tasklister", "Self Help", 1500, "03/05/2018", "09/05/2018")
